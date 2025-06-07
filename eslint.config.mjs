1065c216a9e7e8a5e4bf6a729390dd44a9e78c5b
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-06-07 11:15:38 +0900

    feat: リンターフォーマッターの追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...tailwind.configs["flat/recommended"],
 ];
 
 export default eslintConfig;
