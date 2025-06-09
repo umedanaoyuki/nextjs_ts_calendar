4445e2ed8e96357fbb217f70d1fc4ceebeaad557
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-06-09 22:33:31 +0900

    feat: Cardコンポーネントの導入

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
 
 const SignupPage = () => {
   return (
     <div>
-      
+        <Card>
+            <CardHeader>
+                <CardTitle>ユーザー登録</CardTitle>
+                <CardDescription>ユーザーを作成してください</CardDescription>
+                <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+                <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+                <p>Card Footer</p>
+            </CardFooter>
+        </Card>
     </div>
   )
 }
